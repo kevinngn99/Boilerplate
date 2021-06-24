36f8c3fd6b650038bbb88a66fe36657e4d09c77c
Kevin <kevinnguyen1@ufl.edu>
2021-06-24 03:16:47 -0400

    Basic Project

---
+++ pages/_app.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import type { AppProps } from "next/app";
+import "../styles/main.scss";
+
+const App = ({ Component, pageProps }: AppProps): JSX.Element => {
+  return <Component {...pageProps} />;
+};
+
+export default App;
---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Index = (): JSX.Element => {
+  return (
+    <div>
+      <div>Hello</div>
+    </div>
+  );
+};
+
+export default Index;
